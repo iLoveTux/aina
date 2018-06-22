ab4ffe00d678262398871b1cfe34a042aed92ee8
ilovetux <me@ilovetux.com>
2018-06-22 14:03:11 -0400

    Fix issue where --end-lines was only executed when all tests pass, begin-lines provides this functionality end-lines will always be executed for every line regardless of test statu

--- tests/test_flume.py
+++ tests/test_flume.py
@@ -2,29 +2,108 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `flume` package."""
-
-
 import unittest
+import logging
+from flume.flume import cli
 from click.testing import CliRunner
 
-from flume.flume import cli
 
-
-class TestFlume(unittest.TestCase):
-    """Tests for `flume` package."""
+class TestFlumeStream(unittest.TestCase):
+    """Tests for `flume stream` subcommand."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        # Need this to overcome an issue with testing
+        # basically clearing configuration from the last
+        # call to invoke because, i think, that between
+        # calls sys.stdout is replaced but logging configuration
+        # persists through runs leaving a stale reference to
+        # the old sys.stdout
+        logging.getLogger("").handlers = []
+
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
-        """Test something."""
+    def test_simple_substitution(self):
+        """Test that simple substitution works."""
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            args=(
+                "stream",
+                "--templates",
+                "{{line.decode()}}"
+            ),
+            input="foo"
+        )
+        expected = "foo\n"
+        self.assertEqual(expected, result.output)
 
-    def test_command_line_interface(self):
+    def test_begin_lines_only_executed_when_tests_pass(self):
+        """Test `--begin-lines` is only executed when all tests pass."""
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            args=(
+                "stream",
+                "--tests", "'bar' in line.decode()",
+                "--begin-lines", "print(line.decode(), end='')"
+            ),
+            input="foo\nbar\n"
+        )
+        expected = "bar\n"
+        self.assertEqual(expected, result.output)
+
+    def test_end_lines_only_executed_when_tests_pass(self):
+        """Test `--end-lines` is only executed regardless of the results of test."""
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            args=(
+                "stream",
+                "--tests", "'bar' in line.decode().strip()",
+                "--end-lines", "print(line.decode().strip())",
+            ),
+            input="foo\nbar\n"
+        )
+        expected = "foo\nbar\n"
+        self.assertEqual(expected, result.output)
+
+    def test_only_renders_if_test_pass(self):
+        """Test that templates only render if test passes."""
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            args=(
+                "stream",
+                "--tests", "'bar' in line.decode()",
+                "--templates", "{{line.decode()}}"
+            ),
+            input="foo\nbar"
+        )
+        expected = "bar\n"
+        self.assertEqual(expected, result.output)
+
+    def test_only_renders_if_all_tests_pass(self):
+        """Test that templates only render if all tests passes."""
+        runner = CliRunner()
+        result = runner.invoke(
+            cli,
+            args=(
+                "stream",
+                "--tests", "'bar' in line.decode()",
+                "--tests", "'foo' in line.decode()",
+                "--templates", "{{line.decode()}}",
+            ),
+            input="foo\nbar\nfoobar",
+        )
+        expected = "foobar\n"
+        self.assertEqual(expected, result.output)
+
+    def test_help_menu(self):
         """Test the CLI."""
         runner = CliRunner()
-        help_result = runner.invoke(cli, ['--help'])
+        help_result = runner.invoke(cli, ['stream', '--help'])
         self.assertEqual(help_result.exit_code, 0)
-        self.assertIn('--help  Show this message and exit.', help_result.output)
+        self.assertIn('Show this message and exit.', help_result.output)
