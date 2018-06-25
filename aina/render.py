9085a7e0837bc784f8b2e3f3139722ceb94a3258
ilovetux <me@ilovetux.com>
2018-06-25 11:50:38 -0400

    Add logging into render function

--- aina/render.py
+++ aina/render.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 from hashlib import sha256
+import logging
 import click
 import re
 
@@ -9,17 +10,23 @@ expressions = re.compile(r"(\{%(.*?)%\})", re.DOTALL)
 def render(template, namespace=None):
     global cache
     if namespace is None:
+        logging.info("No namespace given, creating empty namespace")
         namespace = {}
 
     out = template
     for expression in expressions.findall(out):
+        logger.debug("Found expression {}, executing with namespace: {}".format(expression[1], namespace))
         exec(dedent(expression[1]).strip(), namespace)
+        logger.debug("Namespace after executing: {}".format(namespace))
         out = out.replace(expression[0], "")
+        logger.debug("output so far: {}".format(out))
     for statement in statements.findall(out):
+        logger.debug("Found statement {}, evaluating with namespace: {}".format(statement[1], namespace))
         out = out.replace(
             statement[0],
             eval(dedent(statement[1]).strip(), namespace)
         )
+        logger.debug("output so far: {}".format(out))
     return out
 
 if __name__ == "__main__":
