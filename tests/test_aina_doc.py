aaf2373e8e52fc48306156b128c4466d52581e0f
ilovetux <me@ilovetux.com>
2018-07-03 09:01:54 -0400

    Fix Python 3.4 build

--- tests/test_aina_doc.py
+++ tests/test_aina_doc.py
@@ -15,8 +15,10 @@ class TestainaDoc(unittest.TestCase):
         """Test that simple substitution works on a file."""
         runner = CliRunner()
         with runner.isolated_filesystem():
-            Path("src").write_text("this is a {{test}}")
-            Path("namespace").write_text("{'test': 'foo'}")
+            with Path("src").open("w") as fp:
+                fp.write("this is a {{test}}")
+            with Path("namespace").open("w") as fp:
+                fp.write("{'test': 'foo'}")
             runner.invoke(
                 cli,
                 args=(
@@ -26,7 +28,7 @@ class TestainaDoc(unittest.TestCase):
                     "dst",
                 )
             )
-            result = Path("dst").read_text()
+            result = Path("dst").open("r").read()
             expected = "this is a foo"
             self.assertEqual(expected, result)
 
@@ -35,8 +37,10 @@ class TestainaDoc(unittest.TestCase):
         works on a file."""
         runner = CliRunner()
         with runner.isolated_filesystem():
-            Path("src").write_text("{% import re %}{{str(re.findall('\w+', 'this is a test'))}}")
-            Path("namespace").write_text("{'test': 'foo'}")
+            with Path("src").open("w") as fp:
+                fp.write("{% import re %}{{str(re.findall('\w+', 'this is a test'))}}")
+            with Path("namespace").open("w") as fp:
+                fp.write("{'test': 'foo'}")
             runner.invoke(
                 cli,
                 args=(
@@ -46,7 +50,7 @@ class TestainaDoc(unittest.TestCase):
                     "dst",
                 )
             )
-            result = Path("dst").read_text()
+            result = Path("dst").open("r").read()
             expected = "['this', 'is', 'a', 'test']"
             self.assertEqual(expected, result)
 
@@ -58,9 +62,14 @@ class TestainaDoc(unittest.TestCase):
             here = Path(".")
             Path(here / "dst").mkdir()
             Path(here / "src").mkdir()
-            Path(here / "src" / "first").write_text("this is a {{test}}")
-            Path(here / "src" / "second").write_text("this is another {{test}}")
-            Path("namespace").write_text("{'test': 'foo'}")
+
+            with Path(here / "src" / "first").open("w") as fp:
+                fp.write("this is a {{test}}")
+            with Path(here / "src" / "second").open("w") as fp:
+                fp.write("this is another {{test}}")
+            with Path(here / "namespace").open("w") as fp:
+                fp.write("{'test': 'foo'}")
+
             runner.invoke(
                 cli,
                 args=(
@@ -70,7 +79,8 @@ class TestainaDoc(unittest.TestCase):
                     "dst",
                 )
             )
-            first_result = Path(here / "dst" / "first").read_text()
-            second_result = Path(here / "dst" / "second").read_text()
+            print(list(Path(here / "src").iterdir()))
+            first_result = Path(here / "dst" / "first").open("r").read()
+            second_result = Path(here / "dst" / "second").open("r").read()
             self.assertEqual("this is a foo", first_result)
             self.assertEqual("this is another foo", second_result)
